dde3af36ca3c3fbd790d9fe516e6c344260106f8
ptelman <piotr.telman@gmail.com>
2017-04-03 21:44:06 +0200

    Added init EvaluationLogic implementation

--- BioInf/Logic/EvaluationLogic.cs
+++ BioInf/Logic/EvaluationLogic.cs
@@ -9,9 +9,38 @@ namespace BioInf.Logic
 {
     public static class EvaluationLogic
     {
-        public static float Evaluate(Result item, string[] data)
+        public static float Evaluate(Result item)
         {
-            return 1;
+            int max = 0;
+            for (int i = 0; i<item.sequenceIndexes.Length; i++)
+            {
+                int subResult = EvaluationLogic.HandleFromPositionToMax(item, i);
+                if (subResult > max)
+                    max = subResult;
+            }
+            return max;
+        }
+
+        private static int HandleFromPositionToMax(Result item, int startingPosition)
+        {
+            int position = 0;
+            int nucleotidsCounter = startingPosition;
+            while (nucleotidsCounter < item.sequenceIndexes.Length - 1 || position > Global.MaxLength)
+            {
+                position += EvaluationLogic.GetSinglePartialSum(Global.Nucleotids[item.sequenceIndexes[nucleotidsCounter]],
+                    Global.Nucleotids[item.sequenceIndexes[nucleotidsCounter + 1]]);
+                nucleotidsCounter++;
+            }
+            return nucleotidsCounter;
+        }
+
+        private static int GetSinglePartialSum(Nucleotid parentNucleotid, Nucleotid childNucleotid)
+        {
+            int result = 1;
+            while (!childNucleotid.StartsWith(parentNucleotid.Sequence.Substring(result), Global.ErrorToleration))
+                result++;
+
+            return result;
         }
     }
 }
---
+++ BioInf/Model/Global.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioInf.Model
+{
+    public static class Global
+    {
+        public static List<Nucleotid> Nucleotids { get; set; }
+        public static int ErrorToleration { get; set; }
+        public static int MaxLength { get; set; }
+    }
+}

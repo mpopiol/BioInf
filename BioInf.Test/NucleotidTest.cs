ca3132be3bed6aa0834a96694e9ede5fe5c85ff1
Mariusz Popiół <popiolmariusz@gmail.com>
2017-04-03 21:19:22 +0200

    Test project added

---
+++ BioInf.Test/NucleotidTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BioInf.Model;
+using NUnit.Framework;
+
+namespace BioInf.Test
+{
+
+    [TestFixture]
+    public class NucleotidTest
+    {
+        private Nucleotid nucleotid;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            nucleotid = new Nucleotid();
+        }
+
+
+        [Test]
+        public void Nucleotid_StartsWith_ReturnsFalse_OnEmptySubSeq()
+        {
+            nucleotid.Sequence = "ACCCG";
+            var testSubstring = "";
+            var result = nucleotid.StartsWith(testSubstring);
+            
+            Assert.False(result);
+        }
+
+        [Test]
+        public void Nucleotid_StartsWith_ReturnsFalse_OnEmptySeq()
+        {
+            nucleotid.Sequence = "";
+            var testSubstring = "AG";
+            var result = nucleotid.StartsWith(testSubstring);
+
+            Assert.False(result);
+        }
+
+        [Test]
+        public void Nucleotid_StartsWith_ReturnsTrue_OnMatchingWholeSeq()
+        {
+            nucleotid.Sequence = "ACCTGGG";
+            var testSubstring = "ACC";
+            var result = nucleotid.StartsWith(testSubstring);
+
+            Assert.True(result);
+        }
+
+        [Test]
+        public void Nucleotid_StartsWith_ReturnsTrue_MatchingTwoDeltaOne()
+        {
+            nucleotid.Sequence = "ACCTGGG";
+            var testSubstring = "ACG";
+            var result = nucleotid.StartsWith(testSubstring, 1);
+
+            Assert.True(result);
+        }
+
+        [Test]
+        public void Nucleotid_EndsWith_ReturnsFalse_OnEmptySeq()
+        {
+            nucleotid.Sequence = "";
+            var testSubstring = "ACG";
+            var result = nucleotid.EndsWith(testSubstring);
+        }
+    }
+}
--- BioInf/Model/Nucleotid.cs
+++ BioInf/Model/Nucleotid.cs
@@ -10,14 +10,45 @@ namespace BioInf.Model
     {
         public string Sequence { get; set; }
 
-        public bool StartsWith(string subSequence)
+        public bool StartsWith(string subSequence, int delta = 0)
         {
-            throw new NotImplementedException();
+            int errors = 0;
+
+            if ((subSequence.Length <= 0) ||
+                (Sequence.Length <= 0) ||
+                (Sequence[0] != subSequence[0]))
+                return false;
+
+            for (int i=1; i<subSequence.Length; i++)
+            {
+                if (Sequence[i] != subSequence[i])
+                    errors++;
+            }
+
+            if (errors > delta)
+                return false;
+            else
+                return true;
         }
 
-        public bool EndsWith(string subSequence)
+        public bool EndsWith(string subSequence, int delta = 0)
         {
-            throw new NotImplementedException();
+            int errors = 0;
+            int startingPoint = Sequence.Length - subSequence.Length;
+
+            if (Sequence[startingPoint] != subSequence[0])
+                return false;
+
+            for (int i = startingPoint + 1; i < Sequence.Length; i++)
+            {
+                if (Sequence[i] != subSequence[i - startingPoint])
+                    errors++;
+            }
+
+            if (errors > delta)
+                return false;
+            else
+                return true;
         }
     }
 }

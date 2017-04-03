8ef531afe4bc223d8c4ce68189c19e28e4fec08c
ptelman <piotr.telman@gmail.com>
2017-04-03 22:01:56 +0200

    Added init CrossingLogic implementation

--- BioInf/Logic/CrossingLogic.cs
+++ BioInf/Logic/CrossingLogic.cs
@@ -11,7 +11,39 @@ namespace BioInf.Logic
     {
         public static Result Cross(Result item1, Result item2)
         {
-            throw new NotFiniteNumberException();
+            Result result = new Result()
+            {
+                sequenceIndexes = new int[item1.sequenceIndexes.Length]
+            };
+
+            int crossingPoint = Global.Random.Next(1, item1.sequenceIndexes.Length - 2);
+
+            FillBeginning(ref result, item1, crossingPoint);
+            FillEnding(ref result, item2, crossingPoint);
+
+            return result;
+        }
+
+        private static void FillBeginning(ref Result result, Result itemToCross, int crossingPoint)
+        {
+            for (int i=0; i<crossingPoint; i++)
+            {
+                result.sequenceIndexes[i] = itemToCross.sequenceIndexes[i];
+            }
+        }
+
+        private static void FillEnding(ref Result result, Result itemToCross, int crossingPoint)
+        {
+            int resultCounter = crossingPoint;
+            int itemToCrossCounter = 0;
+            while(resultCounter < itemToCross.sequenceIndexes.Length)
+            {
+                if (result.sequenceIndexes.Take(resultCounter).Contains(itemToCross.sequenceIndexes[itemToCrossCounter]))
+                {
+                    result.sequenceIndexes[resultCounter++] = itemToCross.sequenceIndexes[itemToCrossCounter];
+                }
+                itemToCrossCounter++;
+            }
         }
     }
 }

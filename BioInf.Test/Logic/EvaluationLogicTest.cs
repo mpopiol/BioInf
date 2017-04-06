d13ec5736baafa0f8f1ddcf0bac642ff7fe32638
Mariusz Popiół <popiolmariusz@gmail.com>
2017-04-06 23:21:12 +0200

    tests + changes due to errors exposed by tests

---
+++ BioInf.Test/Logic/EvaluationLogicTest.cs
@@ -0,0 +1,345 @@
+﻿using BioInf.Logic;
+using BioInf.Model;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioInf.Test.Logic
+{
+    [TestFixture]
+    public class EvaluationLogicTest
+    {
+        [Test]
+        public void Evaluation_BasicExample()
+        {
+            Global.Nucleotids = new List<Nucleotid>() 
+            { 
+                new Nucleotid()
+                {
+                    Sequence = "ATTTT"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "AATTT"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "AAATT"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "AAAAT"
+                }
+            };
+
+            Global.MaxLength = 8;
+            Global.ErrorToleration = 0;
+            Result res = new Result()
+            {
+                sequenceIndexes = new int[]{4,3,2,1}
+            };
+            var result = EvaluationLogic.Evaluate(res);
+            Assert.AreEqual(4, result);
+        }
+
+        [Test]
+        public void Evaluation_BasicExample2()
+        {
+            Global.Nucleotids = new List<Nucleotid>() 
+            { 
+                new Nucleotid()
+                {
+                    Sequence = "ATTTT"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "AATTT"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "AAATT"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "AAAAT"
+                }
+            };
+
+            Global.MaxLength = 7;
+            Global.ErrorToleration = 0;
+            Result res = new Result()
+            {
+                sequenceIndexes = new int[] { 4, 3, 2, 1 }
+            };
+            var result = EvaluationLogic.Evaluate(res);
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void Evaluation_BasicExample3()
+        {
+            Global.Nucleotids = new List<Nucleotid>() 
+            { 
+                new Nucleotid()
+                {
+                    Sequence = "ATTTT"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "AATTT"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "AAATT"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "AAAAT"
+                }
+            };
+
+            Global.MaxLength = 6;
+            Global.ErrorToleration = 0;
+            Result res = new Result()
+            {
+                sequenceIndexes = new int[] { 4, 3, 2, 1 }
+            };
+            var result = EvaluationLogic.Evaluate(res);
+            Assert.AreEqual(2, result);
+        }
+
+        
+
+        [Test]
+        public void Evaluation_BasicExample4()
+        {
+            Global.Nucleotids = new List<Nucleotid>() 
+            { 
+                
+                new Nucleotid()
+                {
+                    Sequence = "GGTTT"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "TTAAC"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "TAACG"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "GATCA"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "CAGGG"
+                }
+            };
+
+            Global.MaxLength = 9;
+            Global.ErrorToleration = 0;
+            Result res = new Result()
+            {
+                sequenceIndexes = new int[] { 4,5,1,2,3 }
+            };
+            var result = EvaluationLogic.Evaluate(res);
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void Evaluation_BasicExample5()
+        {
+            Global.Nucleotids = new List<Nucleotid>() 
+            { 
+                
+                new Nucleotid()
+                {
+                    Sequence = "GGTTT"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "TTAAC"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "TAACG"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "GATCA"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "CAGGG"
+                }
+            };
+
+            Global.MaxLength = 8;
+            Global.ErrorToleration = 0;
+            Result res = new Result()
+            {
+                sequenceIndexes = new int[] { 3, 4, 5, 1, 2 }
+            };
+            var result = EvaluationLogic.Evaluate(res);
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void Evaluation_BasicExample6()
+        {
+            Global.Nucleotids = new List<Nucleotid>() 
+            { 
+                
+                new Nucleotid()
+                {
+                    Sequence = "QWER"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "TYUI"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "ASDF"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "ZXCV"
+                }
+            };
+
+            Global.MaxLength = 8;
+            Global.ErrorToleration = 0;
+            Result res = new Result()
+            {
+                sequenceIndexes = new int[] { 1,2,4,3 }
+            };
+            var result = EvaluationLogic.Evaluate(res);
+            Assert.AreEqual(2, result);
+
+            Global.MaxLength = 9;
+            Global.ErrorToleration = 0;
+            res = new Result()
+            {
+                sequenceIndexes = new int[] { 1, 2, 4, 3 }
+            };
+            result = EvaluationLogic.Evaluate(res);
+            Assert.AreEqual(2, result);
+
+            Global.MaxLength = 7;
+            Global.ErrorToleration = 0;
+            res = new Result()
+            {
+                sequenceIndexes = new int[] { 1, 2, 4, 3 }
+            };
+            result = EvaluationLogic.Evaluate(res);
+            Assert.AreEqual(1, result);
+        }
+
+        #region DELTA
+        [Test]
+        public void Evaluation_BasicExample_WithDelta()
+        {
+            Global.Nucleotids = new List<Nucleotid>() 
+            { 
+                new Nucleotid()
+                {
+                    Sequence = "GCATT"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "ATTGC"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "TGGCC"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "GGCCA"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "GCCAA"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "CAATT"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "ATTTG"
+                }
+            };
+
+            Global.MaxLength = 8;
+            Global.ErrorToleration = 1;
+            Result res = new Result()
+            {
+                sequenceIndexes = new int[] { 1, 2, 3, 4, 5, 6, 7 }
+            };
+            var result = EvaluationLogic.Evaluate(res);
+            Assert.AreEqual(4, result);
+        }
+
+        [Test]
+        public void Evaluation_BasicExample_WithDelta2()
+        {
+            Global.Nucleotids = new List<Nucleotid>() 
+            { 
+                new Nucleotid()
+                {
+                    Sequence = "GCATT"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "ATTGC"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "TGGCC"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "TGCCA"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "GCCAA"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "CAATT"
+                },
+                new Nucleotid()
+                {
+                    Sequence = "ATTGG"
+                }
+            };
+
+            Global.MaxLength = 8;
+            Global.ErrorToleration = 1;
+            Result res = new Result()
+            {
+                sequenceIndexes = new int[] { 1, 2, 3, 4, 5, 6, 7 }
+            };
+            var result = EvaluationLogic.Evaluate(res);
+            Assert.AreEqual(3, result);
+
+            Global.MaxLength = 9;
+            Global.ErrorToleration = 1;
+            res = new Result()
+            {
+                sequenceIndexes = new int[] { 1, 2, 3, 4, 5, 6, 7 }
+            };
+            result = EvaluationLogic.Evaluate(res);
+            Assert.AreEqual(3, result);
+        }
+        #endregion
+    }
+}
--- BioInf/Logic/EvaluationLogic.cs
+++ BioInf/Logic/EvaluationLogic.cs
@@ -9,7 +9,7 @@ namespace BioInf.Logic
 {
     public static class EvaluationLogic
     {
-        public static float Evaluate(Result item)
+        public static int Evaluate(Result item)
         {
             int max = 0;
             for (int i = 0; i<item.sequenceIndexes.Length; i++)
@@ -23,21 +23,22 @@ namespace BioInf.Logic
 
         private static int HandleFromPositionToMax(Result item, int startingPosition)
         {
-            int position = 0;
+            int position = Global.Nucleotids[0].Sequence.Length;
             int nucleotidsCounter = startingPosition;
-            while (nucleotidsCounter < item.sequenceIndexes.Length - 1 || position > Global.MaxLength)
+            while (nucleotidsCounter < item.sequenceIndexes.Length - 1 && position < Global.MaxLength)
             {
-                position += EvaluationLogic.GetSinglePartialSum(Global.Nucleotids[item.sequenceIndexes[nucleotidsCounter]],
-                    Global.Nucleotids[item.sequenceIndexes[nucleotidsCounter + 1]]);
-                nucleotidsCounter++;
+                position += EvaluationLogic.GetSinglePartialSum(Global.Nucleotids[item.sequenceIndexes[nucleotidsCounter] - 1],
+                    Global.Nucleotids[item.sequenceIndexes[nucleotidsCounter + 1] - 1]);
+                if(position <= Global.MaxLength)
+                    nucleotidsCounter++;
             }
-            return nucleotidsCounter;
+            return nucleotidsCounter + 1 - startingPosition;
         }
 
         private static int GetSinglePartialSum(Nucleotid parentNucleotid, Nucleotid childNucleotid)
         {
             int result = 1;
-            while (!childNucleotid.StartsWith(parentNucleotid.Sequence.Substring(result), Global.ErrorToleration))
+            while (!childNucleotid.StartsWith(parentNucleotid.Sequence.Substring(result), Global.ErrorToleration) && result < parentNucleotid.Sequence.Length)
                 result++;
 
             return result;

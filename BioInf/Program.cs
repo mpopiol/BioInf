2c60be57b5319acdd7b25a8ed184319cbada4785
ptelman <piotr.telman@gmail.com>
2017-04-17 14:33:41 +0200

    Everything parametrised

--- BioInf/Program.cs
+++ BioInf/Program.cs
@@ -14,11 +14,17 @@ namespace BioInf
         {
             InitData();
 
+            int instanceSize = 200;
+            int populationSize = 50;
+            float maxMutationPercentage = 0.85f;
+            float mutationPercentage = 0.1f;
+            int mutationsPlusCrossing = instanceSize - populationSize;
+
             var streamWriter = new StreamWriter("output.txt");
 
-            Result[] population = new Result[200];
+            Result[] population = new Result[instanceSize];
 
-            for (int i = 0; i < 50; i++)
+            for (int i = 0; i < populationSize; i++)
             {
                 population[i] = new Result()
                 {
@@ -30,25 +36,31 @@ namespace BioInf
             while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
             {
                 j++;
-                Parallel.For(50, 125, i =>
+
+                if (j % 100 == 0 && mutationPercentage < maxMutationPercentage)
+                    mutationPercentage += 0.01f;
+
+                int mutations = (int)(mutationsPlusCrossing * mutationPercentage);
+
+                Parallel.For(populationSize, populationSize + mutations, i =>
                 {
-                    population[i] = MutationLogic.Mutate(population[Global.Random.Next(49)]);
+                    population[i] = MutationLogic.Mutate(population[Global.Random.Next(populationSize - 1)]);
                     //for (int k = 0; k < (int)(j / 500); k++)
                     //{
                     //    population[i] = MutationLogic.Mutate(population[i]);
                     //}
                 });
 
-                Parallel.For(125, 200, i =>
+                Parallel.For(populationSize + mutations, instanceSize, i =>
                 {
-                    population[i] = CrossingLogic.Cross(population[Global.Random.Next(49)], population[Global.Random.Next(49)]);
+                    population[i] = CrossingLogic.Cross(population[Global.Random.Next(populationSize - 1)], population[Global.Random.Next(populationSize - 1)]);
                     //for (int k = 0; k < (int)(j / 1500); k++)
                     //{
                     //    population[i] = CrossingLogic.Cross(population[i], population[Global.Random.Next(49)]);
                     //}
                 });
 
-                Parallel.For(0, 200, i =>
+                Parallel.For(0, instanceSize, i =>
                 {
                     population[i].EvaluationPoints = EvaluationLogic.Evaluate(population[i]);
                     population[i].TotalLength = EvaluationLogic.GetTotalLength(population[i]);
